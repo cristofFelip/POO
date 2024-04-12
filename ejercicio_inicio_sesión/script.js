c7c05b63c944cd5e0546876531ee069aeb5583b0
cristofFelip <cristobalcornejo@liceovvh.cl>
2024-04-12 12:03:21 -0400

    ok

--- ejercicio_inicio_sesión/script.js
+++ ejercicio_inicio_sesión/script.js
@@ -2,7 +2,10 @@ function inicioSesion(){
     let nombreUsuario = prompt("Ingrese su nombre de usuario: ");
     let Contraseña = prompt("Ingrese su contraseña: ");
 
-    if (nombreUsuario == "usuario123" && Contraseña == "secreto"){
+    const usuario = "usuario123"
+    const contr = "secreto"
+
+    if (nombreUsuario == usuario && Contraseña == contr){
         nombreUsuario = document.getElementById("lista").innerHTML = "<h2>¡Acceso concedido!</h2>";
     } else {
         Contraseña = document.getElementById("lista").innerHTML = "<h2>Acceso denegado :c</h2>";
