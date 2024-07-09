e925c1b48beed236f39471536ec820cf925cc11a
cristofFelip <cristobalcornejo@liceovvh.cl>
2024-07-09 09:32:06 -0400

    ok

--- 01_ejercicios_clicks/script.js
+++ 01_ejercicios_clicks/script.js
@@ -2,5 +2,5 @@ function click(elemento){
     let contador =parseInt(element.getAttribute('data-contador') || '0');
     contador++;
     elemento.setAttribute('data.contador', contador);
-    elemento.textContent = 'Clics' + contador;
+    elemento.textContent = 'Clics ' + contador;
 }
