f6fd7b687630257b9d24416992fc415fafb18719
cristofFelip <cristobalcornejo@liceovvh.cl>
2024-07-11 12:33:21 -0400

    ok

--- 01_ejercicios_clicks/script.js
+++ 01_ejercicios_clicks/script.js
@@ -1,6 +1,7 @@
-function click(elemento){
-    let contador =parseInt(element.getAttribute('data-contador') || '0');
+function contador(elemento){
+    console.log("funcion click");
+    let contador = parseInt(elemento.getAttribute('data-contador') || '0');
     contador++;
-    elemento.setAttribute('data.contador', contador);
+    elemento.setAttribute('data-contador', contador);
     elemento.textContent = 'Clics ' + contador;
 }
