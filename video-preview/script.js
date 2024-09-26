3125e5d197f01eab2c99948f9465687ac302d300
cristofFelip <cristobalcornejo@liceovvh.cl>
2024-09-26 13:02:03 -0300

    ok

--- video-preview/script.js
+++ video-preview/script.js
@@ -1,10 +1,18 @@
 console.log("page loaded...");
 
 
-function playVideo(video){
-    video.play();
+function playVideo(secondaryVideo){
+    var mainVideo = document.getElementById("mainVideo");
+    var temp = mainVideo.src;
+    mainVideo.src = secondaryVideo.src;
+    mainVideo.play();
+    secondaryVideo.src = temp;
 };
 
-function pauseVideo(video){
-    video.pause();
+function pauseVideo(){
+    var mainVideo = document.getElementById("mainVideo");
+    var tempSrc = secondaryVideo.src;
+    secondaryVideo.src = mainVideo.src;
+    mainVideo.src = tempSrc;
+    mainVideo.play();
 };
